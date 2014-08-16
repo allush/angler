80708acebbfbd1ce6209f4380aba57debf7ec2ee
user12va1 <nikulin_na@mail.ru>
2014-08-16 13:49:33 +0400

    Поправка комментария в форме

--- www/protected/models/RegisterForm.php
+++ www/protected/models/RegisterForm.php
@@ -1,15 +1,15 @@
 <?php
 
 /**
- * LoginForm class.
- * LoginForm is the data structure for keeping
- * user login form data. It is used by the 'login' action of 'SiteController'.
+ * RegisterForm class.
+ * user register form data. It is used by the 'register' action of 'SiteController'.
  */
 class RegisterForm extends CFormModel
 {
 	public $username;
 	public $email;
 	public $password;
+private $_identity;
 
 }
 ?>
