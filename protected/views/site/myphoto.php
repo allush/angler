b19f955bda197b2f192f8176fbca129c6ec126a0
Nikulin <fakemail@fake.com>
2014-08-27 16:09:21 +0400

    myphoto

--- protected/views/site/myphoto.php
+++ protected/views/site/myphoto.php
@@ -5,14 +5,17 @@ $this->pageTitle=Yii::app()->name . ' - My Photo';
 $this->breadcrumbs=array(
     'My Photo',
 );
-$user=Photo::model()->findByAttributes(array('id' => Yii::app()->user->id));
+$user=Photo::model()->findByAttributes(array('user_id' => Yii::app()->user->id));
 ?>
 
 <h1>My photo</h1>
 
 <p>Here you can see all your photo</p>
 
-<img src="images/photo/5c2591ead689e54f972fca7fafb98247.jpg" width="100%">
+<?php
+CHtml::image($user->path().$user->filename);
+?>
+
 
 
 
