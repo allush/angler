6715811caf2e1a21d71b7ef71662d51efbc5b67c
user12va1 <nikulin_na@mail.ru>
2014-08-29 14:27:25 +0400

    Добавление bootstrap, перенос приложения

---
+++ www/protected/views/site/pages/about.php
@@ -0,0 +1,14 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name . ' - About';
+$this->breadcrumbs=array(
+	'About',
+);
+?>
+<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+<script src="js/bootstrap.min.js"></script>
+<h1>About</h1>
+
+<p>This is a "static" page. You may change the content of this page
+by updating the file <code><?php echo __FILE__; ?></code>.</p>
