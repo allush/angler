9c8523cf63fb0d6c298574a1b99be2c4e2d008bf
Nikulin <fakemail@fake.com>
2014-10-10 15:47:50 +0400

    Интеграция с соцсетями с высылкой пароля

--- protected/components/UloginUserIdentity.php
+++ protected/components/UloginUserIdentity.php
@@ -26,29 +26,38 @@ class UloginUserIdentity implements IUserIdentity
             $this->id = $user->id;
             $this->email = $user->email;
         } else {
-            $user = new User();
-//            $mail = new PHPMailer();
-//            $mail->setLanguage('ru');
-            $user->identity = $uloginModel->identity;
-            $user->network = $uloginModel->network;
-            $user->email = $uloginModel->email;
-            $user->username = $uloginModel->email;
-            $pass = rand(100, 1000000000);
-            $user->password = (string)$pass;
-//
-//            $mail->From = 'angler@angler.com';
-//            $mail->FromName = 'Angler';
-//            $mail->addAddress($uloginModel->email); // Add a recipient
-//
-//            $mail->WordWrap = 50; // Set word wrap to 50 characters
-//            $mail->isHTML(true); // Set email format to HTML
-//            mail($uloginModel->email, 'Профиль', 'Поздравляем с регистрацией, ваш пароль <b>' . $user->password . '</b> Для его смены перейдите по ссылке: <a href=' . Yii::app()->homeUrl . 'Ваш профиль</a>');
-//            $mail->Subject = 'Пароль';
-//            $mail->Body = 'Поздравляем с регистрацией, ваш пароль <b>' . $user->password . '</b> Для его смены перейдите по ссылке: <a href=' . Yii::app()->homeUrl . 'Ваш профиль</a>';
-//
-//
-//            $mail->send();
-            $user->save();
+            $transaction = Yii::app()->db->getCurrentTransaction();
+            if (!$transaction) {
+                $transaction = Yii::app()->db->beginTransaction();
+            }
+            try {
+                $user = new User();
+                $user->identity = $uloginModel->identity;
+                $user->network = $uloginModel->network;
+                $user->email = $uloginModel->email;
+                $user->username = $uloginModel->email;
+                $pass = rand(100, 1000000000);
+                $user->password = (string)$pass;
+                if ($user->save()) {
+                    $mail = new PHPMailer();
+                    $mail->setLanguage('ru');
+                    $mail->addAddress($uloginModel->email); // Add a recipient
+                    $mail->isHTML(true); // Set email format to HTML
+                    $mail->CharSet = 'utf-8';
+                    $mail->From = 'angler@angler.com';
+                    $mail->FromName = 'Angler';
+                    $mail->WordWrap = 50; // Set word wrap to 50 characters
+                    $mail->Subject = 'Пароль';
+                    $mail->Body = 'Поздравляем с регистрацией, ваш пароль <b>' . $user->password . '</b> Для его смены перейдите по ссылке: <a href=' . Yii::app()->createAbsoluteUrl('profile/profile') . '</a>';
+                    if(!$mail->send()){
+                        throw new CException('Сообщение не сохранилось, пользователь не создан.');
+                    }
+                }
+                $transaction->commit();
+            } catch (CException $e) {
+                $transaction->rollback();
+            }
+
 
             $this->id = $user->id;
             $this->email = $user->email;
