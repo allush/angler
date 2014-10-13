c72d1c4784d29a02ba4e82d2d85a6a6b41161292
MaxSomov <somov-93@mail.ru>
2014-10-13 16:57:23 +0400

    Boostbox. Сворачивающиеся основное меню

--- protected/modules/admin/views/layouts/default.php
+++ protected/modules/admin/views/layouts/default.php
@@ -51,6 +51,11 @@
 <script src="/js/boostbox/libs/jquery/jquery-migrate-1.2.1.min.js"></script>
 <script src="/js/boostbox/core/BootstrapFixed.js"></script>
 <script src="/js/boostbox/libs/bootstrap/bootstrap.min.js"></script>
+<script src="../assets/js/libs/jquery-ui/jquery-ui-1.10.3.custom.min.js"></script>
+<script src="../assets/js/libs/spin.js/spin.min.js"></script>
+<script src="../assets/js/libs/slimscroll/jquery.slimscroll.min.js"></script>
+<script src="../assets/js/core/demo/DemoDocs.js"></script>
+<script src="../assets/js/core/demo/Demo.js"></script>
 <!-- Additional JS includes -->
 
 <!-- Always put App.js last in your javascript imports -->
@@ -59,21 +64,50 @@
 
 
 <header id="header">
-    <?php echo CHtml::encode(Yii::app()->name); ?>
+
+
+
+    <!-- BEGIN NAVBAR -->
+    <nav class="navbar navbar-default" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <a class="btn btn-transparent btn-equal btn-menu" href="javascript:void(0);"><i class="fa fa-bars fa-lg"></i></a>
+            <div class="navbar-brand">
+                <a class="main-brand" href="../dashboards/dashboard.html">
+                    <h3 class="text-light text-white"><span>Boost<strong>Box</strong> </span><i class="fa fa-rocket fa-fw"></i></h3>
+                </a>
+            </div><!--end .navbar-brand -->
+            <a class="btn btn-transparent btn-equal navbar-toggle" data-toggle="collapse" data-target="#header-navbar-collapse"><i class="fa fa-wrench fa-lg"></i></a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="header-navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <h1><?php echo CHtml::encode(Yii::app()->name); ?></h1>
+            </ul><!--end .nav -->
+        </div><!--end #header-navbar-collapse -->
+    </nav>
+    <!-- END NAVBAR -->
 </header>
 
 
    <!-- header -->
     <div id="base">
 
-        <div id="sidebar">
+        <div id="sidebar" class="sidebar-fixed">
             <div class="sidebar-back"></div>
             <div class="sidebar-content">
                 <div class="nav-brand">
-                    <a class="main-brand" href="#"><h3 class="text-light text-white">логотип</h3> </a>
+                    <a class="main-brand" href="#">
+                        <h3 class="text-light text-white">
+                            Л<span>оготип</span>
+                        </h3>
+                    </a>
                 </div>
 
-
+<!--каждая новость в отдельном <section> -->
+<!--изменить горизонтальное меню-->
+<!--Обернуть элементы меню в <span> -->
             <?php $this->widget('zii.widgets.CMenu', array(
                 'items' => array(
                     array('label' => 'Админская', 'url' => array('default/index')),
@@ -87,6 +121,7 @@
                     'class'=>'main-menu'
                 )
             )); ?>
+
 
                 </div>
 
@@ -108,8 +143,11 @@
                     'class'=>'nav nav-pills nav-stacked'
                 )
             )); ?>
-
+            <section>
+            <div class="section-body contain-lg">
             <?php echo $content; ?>
+                </div>
+            </section>
 
         </div>
     </div>
