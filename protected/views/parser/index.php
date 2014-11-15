39da3f9e676f49f4818fc0f4d9518737598093b3
MaxSomov <somov-93@mail.ru>
2014-11-15 20:57:40 +0300

    Удалил лишний код

--- protected/views/parser/index.php
+++ protected/views/parser/index.php
@@ -18,42 +18,6 @@ $this->breadcrumbs = array(
 ?>
 
 
-<!--<script type="text/javascript">-->
-<!--    $(document).ready(function () {-->
-<!--        $(".fancybox").fancybox({-->
-<!---->
-<!--        });-->
-<!--    });-->
-<!---->
-<!---->
-<!--    $(function () {-->
-<!--        var time = new Date().getTime() / 1000;-->
-<!--        var date1, date2;-->
-<!--        var datefirst, datelast;-->
-<!--        $("#slider-range").slider({-->
-<!--            range: true,-->
-<!--            min: 1415277424,-->
-<!--            max: time,-->
-<!--            step: 86400,-->
-<!--            values: [ 1415277424, 1415466983],-->
-<!--            slide: function (event, ui) {-->
-<!--                date1 = new Date(ui.values[0] * 1000);-->
-<!--                date2 = new Date(ui.values[1] * 1000);-->
-<!--                $("#amount").val(date1.getDate() + "." + date1.getMonth() + "." + date1.getFullYear() + " - " + date2.getDate() + "." + date2.getMonth() + "." + date2.getFullYear());-->
-<!--                $('[name=from]').val(ui.values[0]);-->
-<!--                $('[name=to]').val(ui.values[1]);-->
-<!--            }-->
-<!--        });-->
-<!--        date1 = new Date(1415277424 * 1000);-->
-<!--        date2 = new Date(1415466983 * 1000);-->
-<!--        $("#amount").val(date1.getDate() + "." + date1.getMonth() + "." + date1.getFullYear() + " - " + date2.getDate() + "." + date2.getMonth() + "." + date2.getFullYear());-->
-<!--        $('[name=from]').val(ui.values[0]);-->
-<!--        $('[name=to]').val(ui.values[1]);-->
-<!--    });-->
-<!---->
-<!---->
-<!--</script>-->
-
 
 <script type="text/javascript">
     $(document).ready(function () {
@@ -108,79 +72,10 @@ $this->breadcrumbs = array(
 
 <?php $this->endWidget(); ?>
 
+<br/>
 
-<form method="get" action="index.php">
-    <input name="r" value="parser/index.php" type="hidden">
 
-    <p>
-        <label for="amount">Диапазон дат:</label>
-        <input type="text" id="amount" readonly style="border:0; color:#f6931f; font-weight:bold;">
-        <input name="from" value="1415277424" type="hidden">
-        <input name="to" value="1415466983" type="hidden">
-    </p>
 
-    <div id="slider-range"></div>
-    <br/>
-<!--    <input type="submit">-->
-
-</form>
-<?php
-//$dataProvider->sort->defaultOrder = "date DESC";
-//
-//
-//$this->widget('zii.widgets.grid.CGridView', array(
-//    'id' => 'parser-grid',
-//    'dataProvider' => $dataProvider,
-//    'summaryText' => '',
-//    'columns' => array(
-//        array(
-//
-//            'name' => 'id',
-//            'header' => '#',
-//            'type' => 'raw',
-//            'value' => '$data->id',
-//        ),
-////        array(
-////            'name' => 'name',
-////            'header' => 'Название сайта',
-////            'type' => 'raw',
-////            'value' => 'CHtml::link($data->name, "/parser/sites/".$data->id.".html", array("class"=>"fancybox", "data-fancybox-type"=>"iframe"))',
-////            'name'=>'id',
-////            'header'=>'#',
-////            'type'=>'raw',
-////            'value'=>'$data->id',
-////        ),
-//        array(
-//            'name' => 'name',
-//            'header' => 'Название сайта',
-//            'type' => 'raw',
-//            'value' => 'CHtml::link($data->name, $data->snapshotUrl(), array("class"=>"fancybox"))',
-//
-//        ),
-//        array(
-//            'name' => 'date',
-//            'header' => 'Дата слепка',
-//            'value' => 'date("d.m.Y", $data->date)',
-//        ),
-//
-//    ),
-//    'itemsCssClass' => 'table table-hover',
-//    'pagerCssClass' => 'eqweqewqeq',
-//    'pager' => array(
-//        'firstPageLabel' => '<<',
-//        'prevPageLabel' => '<',
-//        'nextPageLabel' => '>',
-//        'lastPageLabel' => '>>',
-//        'maxButtonCount' => '7',
-//        'header' => '',
-//        'selectedPageCssClass' => 'active',
-//        'htmlOptions' => array(
-//            'class' => 'pagination',
-//        )
-//    ),
-//    //'htmlOptions'=>array('class'=>'table'),
-//));
-?>
 
 
 <table class="table table-bordered">
